b77b93177e03e72c1155674bb87b844bde4abbf8
Mouad Nachit <mnachit@c4r1p7.1337.ma>
2024-05-27 13:47:48 +0100

    s

---
+++ execution/execve.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execve.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnachit <mnachit@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/26 14:21:17 by moer-ret          #+#    #+#             */
+/*   Updated: 2024/05/26 16:10:53 by mnachit          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	case1(char *tmp, t_node *tree, char **env)
+{
+	if (execve(tmp, tree->data->cmd->args, env) == -1)
+		printf("minishell: command not found\n");
+}
+
+void	case2(t_node *tree, char **env)
+{
+	if (access(tree->data->cmd->args[0], F_OK | X_OK) == 0)
+		execve(tree->data->cmd->args[0], tree->data->cmd->args, env);
+	else
+		printf("minishell: command not found\n");
+}
+
+char	*path_check(char **env)
+{
+	int	i;
+
+	i = 0;
+	while (env[i])
+	{
+		if (ft_strncmp(env[i], "PATH=", 5) == 0)
+			break ;
+		i++;
+	}
+    
+	return (env[i]);
+}
+
+void ft_execute2(t_node *tree, char **env)
+{
+    char	*str;
+	char	**path;
+    char   *tmp;
+	int		i;
+
+	i = 0;
+	str = path_check(env);
+	path = ft_split(str + 5, ':');
+    
+	while (path[i] && tree->data->cmd->args)
+	{
+		if (ft_strchr(tree->data->cmd->args[0], '/') == NULL) ///nfs/homes/moer-ret/bin  /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /usr/games /ls
+		{
+			str = ft_strjoin("/", tree->data->cmd->args[0]);
+			tmp = ft_strjoin(path[i], str);
+			free (str);
+			if (access(tmp, F_OK) == 0)
+				break ;
+			free(tmp);
+		}
+		else
+		{
+			case2(tree, env);
+			break;
+		}
+		i++;
+	}
+	if (ft_strchr(tree->data->cmd->args[0], '/') == NULL)
+		case1(tmp, tree, env);
+}
+
+void	ft_execute(t_node *tree,  char **env)
+{   
+    // int fd[2];
+    int ip1;
+
+    // if (pipe(fd) == -1)
+    //     printf("error\n");
+    ip1 = fork();
+    if (ip1 == 0)
+    {
+        ft_execute2(tree, env);
+    }
+    else
+    {
+        waitpid(ip1, NULL, 0);
+    }
+}
